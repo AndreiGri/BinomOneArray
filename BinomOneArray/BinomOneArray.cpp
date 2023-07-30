25a84a7b142ad2c8588a12682f83a40ca1b0a99c
Gridin_Andrei <andreigridin918@gmail.com>
2023-07-30 20:20:41 +0500

    Добавьте файлы проекта.

---
+++ BinomOneArray/BinomOneArray.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int n, k = 0;
+    double i = 1;
+    cout << "Введите нижний индекс коэффициэнтов n: ";
+    cin >> n;
+    while (k < n) {
+        cout << i << ", ";
+        i *= (double)(n - k) / (k + 1);
+        k++;
+    }
+    cout << i << ".";
+    system("pause > nul");
+    return 0;
+}
